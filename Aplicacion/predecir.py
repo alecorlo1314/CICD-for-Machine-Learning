838787a889e5e06b89458916150162b270dffef2
alecorlo <alecorlo1234@gmail.com>
2026-02-24 00:32:54 -0600

    Se elimino predecir de /Aplicacion

--- Aplicacion/predecir.py
+++
@@ -1,9 +0,0 @@
-import skops.io as sio
-
-
-def load_model():
-    return sio.load("Modelo/pipeline.skops", trusted=["numpy.dtype"])
-
-
-def predict(model, data):
-    return model.predict([data])[0]
