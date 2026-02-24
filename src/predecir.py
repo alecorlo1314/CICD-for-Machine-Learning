015b4d15a0a33552860e9dbf1d4051a04c967d8c
alecorlo <alecorlo1234@gmail.com>
2026-02-23 19:46:46 -0600

    Se actualizo Aplicacion/drug_app.py para el directorio raiz, ademas de poner excepciones en la carpeta de predecir y entrenar en src/

--- src/predecir.py
+++ src/predecir.py
@@ -1,8 +1,14 @@
 import skops.io as sio
 
 def load_model(path: str):
-    return sio.load(path, trusted=["numpy.dtype"])
+    try:
+        return sio.load(path, trusted=["numpy.dtype"])
+    except Exception as e:
+        raise RuntimeError(f"Error al cargar el modelo: {e}") 
 
 
 def predict(model, features: list):
-    return model.predict([features])[0]
+    try:
+        return model.predict([features])[0]
+    except Exception as e:
+        raise RuntimeError(f"Error al realizar la predicción: {e}")
